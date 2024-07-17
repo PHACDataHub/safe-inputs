62ec9d9ac7a4a24edb40a4a0dbaba4347f068d45
Niranjan <niranjanram107@gmail.com>
2024-07-17 10:29:42 -0700

    CSS changes for page layout

--- ui/src/pages/NavWrapper.tsx
+++ ui/src/pages/NavWrapper.tsx
@@ -3,17 +3,20 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from 'src/components/layout/Footer.tsx';
 import Header from 'src/components/layout/Header.tsx';
+
 import NavWrapperErrorFallBack from './error/NavWrapperErrorFallback.tsx';
 
 // This is the component that renders the header and footer for all pages
 export default function NavWrapper() {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      <ErrorBoundary FallbackComponent={NavWrapperErrorFallBack}>
-        <Outlet />
-      </ErrorBoundary>
+      <div className="content-container">
+        <ErrorBoundary FallbackComponent={NavWrapperErrorFallBack}>
+          <Outlet />
+        </ErrorBoundary>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
